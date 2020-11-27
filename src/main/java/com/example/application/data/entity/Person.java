a273159e42085332821cefd90b6329457664508f
Joonas Lehtinen <joonas@vaadin.com>
2020-11-27 09:58:09 -0800

    Use Lombok
    To simplify projects, we could promote use of Lombok. It is well
    supported by all IDEs our audience uses and significantly reduce
    boilerplate code. This is in good alignment with our north star metric
    of making things more simple. Furthermore, we can point to delombok
    for people who do not want to use Lombok.

--- src/main/java/com/example/application/data/entity/Person.java
+++ src/main/java/com/example/application/data/entity/Person.java
@@ -1,12 +1,15 @@
 package com.example.application.data.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 
 import javax.validation.constraints.Email;
 import java.time.LocalDate;
 import javax.annotation.Nullable;
 
-@Entity
+@Entity @Getter @Setter
 public class Person extends AbstractEntity {
 
     private String firstName;
@@ -17,42 +20,5 @@ public class Person extends AbstractEntity {
     @Nullable
     private LocalDate dateOfBirth;
     private String occupation;
-
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPhone() {
-        return phone;
-    }
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-    public String getOccupation() {
-        return occupation;
-    }
-    public void setOccupation(String occupation) {
-        this.occupation = occupation;
-    }
 
 }
