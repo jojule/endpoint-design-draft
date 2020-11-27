316414791b74602cc3f67f4e45f82a7a72d24087
Joonas Lehtinen <joonas@vaadin.com>
2020-11-27 10:22:24 -0800

    Removed CrudEndpoint to simplify generated project
    To keep generated project simple to learn and understand, we should
    stick to basics. When Enpoints are inherited, we fail to teach how
    simple endpoints can be built. If user later on wants to add abstract
    parents to re-use functionality, we should explain that in documentation
    teaching about EndpointExposed there.

--- src/main/java/com/example/application/data/endpoint/PersonEndpoint.java
+++ src/main/java/com/example/application/data/endpoint/PersonEndpoint.java
@@ -1,17 +1,20 @@
 package com.example.application.data.endpoint;
 
-import com.example.application.data.CrudEndpoint;
 import com.example.application.data.entity.Person;
 import com.example.application.data.service.PersonService;
 import com.vaadin.flow.server.connect.Endpoint;
-
+import com.vaadin.flow.server.connect.auth.AnonymousAllowed;
 import org.springframework.beans.factory.annotation.Autowired;
-import javax.validation.constraints.Email;
-import java.time.LocalDate;
-import javax.annotation.Nullable;
+import org.springframework.data.domain.Page;
+import org.vaadin.artur.helpers.GridSorter;
+import org.vaadin.artur.helpers.PagingUtil;
+
+import java.util.List;
+import java.util.Optional;
 
 @Endpoint
-public class PersonEndpoint extends CrudEndpoint<Person, Integer> {
+@AnonymousAllowed
+public class PersonEndpoint {
 
     private PersonService service;
 
@@ -19,9 +22,30 @@ public class PersonEndpoint extends CrudEndpoint<Person, Integer> {
         this.service = service;
     }
 
-    @Override
     protected PersonService getService() {
         return service;
+    }
+
+    public List<Person> list(int offset, int limit, List<GridSorter> sortOrder) {
+        Page<Person> page = getService()
+                .list(PagingUtil.offsetLimitTypeScriptSortOrdersToPageable(offset, limit, sortOrder));
+        return page.getContent();
+    }
+
+    public Optional<Person> get(Integer id) {
+        return getService().get(id);
+    }
+
+    public Person update(Person entity) {
+        return getService().update(entity);
+    }
+
+    public void delete(Integer id) {
+        getService().delete(id);
+    }
+
+    public int count() {
+        return getService().count();
     }
 
 }
